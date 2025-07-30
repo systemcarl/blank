05c1a2b812b25b9cb3f2ccf8be6dc0ad75f2b630
Carl Lyons <code@carledwardlyons.ca>
2025-07-29 21:26:05 -0400

    Configure Sentry Error Monitoring

---
+++ src/hooks.server.ts
@@ -0,0 +1,19 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import * as Sentry from '@sentry/sveltekit';
+import { env } from '$env/dynamic/public';
+
+Sentry.init({
+  dsn : env.PUBLIC_SENTRY_DSN,
+  sendDefaultPii : true,
+});
+
+function errorHandler({ error, event } : {
+  error : unknown;
+  event : RequestEvent;
+}) {
+  console.error('Unhandled error (server):', error, event);
+};
+
+export const handleError = Sentry.handleErrorWithSentry(errorHandler);
+
+export const handle = Sentry.sentryHandle();
