dfbb35116d3687a6202ef5f3c1c31580735e21a8
Carl Lyons <code@carledwardlyons.ca>
2025-08-12 11:39:28 -0400

    Apply Theme CSS Styles
    Compile all themes into server rendered CSS file. Cascade theme
    sections, typography, and graphics variable definitions to minimize
    variable duplication and standardize element styling. Apply current
    theme to the root element, and set the document body to the current
    theme's default section. Generate classes for theme provider elements
    to define variable scope.
    
    Before first paint, set the root class to apply the user selected theme
    to prevent flash of unstyled content during subsequent page loads.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import useThemes from '$lib/hooks/useThemes';
+
+  const { data, children } = $props();
+
+  const { setThemes, subscribeLocalTheme } = useThemes();
+
+  setThemes(data.themes);
+
+  subscribeLocalTheme();
+</script>
+
+{@render children()}
