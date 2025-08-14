b39cb20f6f3c9ee6ae30179680e9654191f386c7
Carl Lyons <code@carledwardlyons.ca>
2025-08-14 11:31:50 -0400

    Preload SVG Graphics
    Fetch all theme SVG graphics before server render and embed XML in
    application data store for client-side injection.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import useThemes from '$lib/hooks/useThemes';
+  import useGraphics from '$lib/hooks/useGraphics';
 
   const { data, children } = $props();
 
   const { setThemes, subscribeLocalTheme } = useThemes();
+  const { setGraphics } = useGraphics();
 
   setThemes(data.themes);
+  setGraphics(data.graphics);
 
   subscribeLocalTheme();
 </script>
