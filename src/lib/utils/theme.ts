systemcarl/folio

Resolve Theme Fonts, Scale Values, and Typography
Query theme fonts with reference to respective import URLs. Resolve
section typography using theme fonts and section scale.

src/lib/utils/theme.ts
@@ -3,10 +3,20 @@ import { log } from '$lib/utils/logs';
 
 export interface Section {
   palette : Palette;
+  scale : Scale;
   background : Background;
+  typography : Record<string, Typography>;
 }
 
 export type Palette = Record<string, string>;
+
+export type Scale = Record<string, string>;
+
+export interface Font {
+  family : string;
+  fontFace? : string;
+  import? : string;
+}
 
 export interface Background {
   img? : {
@@ -18,21 +28,57 @@ export interface Background {
   fill? : string;
 }
 
+export interface Typography {
+  font : string;
+  size : string;
+  lineHeight? : string;
+  letterSpacing? : string;
+  weight? : string;
+  style? : string;
+  textDecoration? : string;
+  colour? : string;
+}
+
 export const defaultTheme = {
   sections : {
     default : {
       palette : 'default',
+      scale : 'default',
       background : 'default',
+      typography : 'default',
     },
   },
   palettes : {
     default : {
       background : '#FFFFFF',
+      textPrimary : '#000000',
     },
+  },
+  scales : {
+    default : {
+      fontSize : '1rem',
+    },
+  },
+  fonts : {
+    default : { family : 'Arial, sans-serif' },
   },
   backgrounds : {
     default : {
       fill : 'background',
+    },
+  },
+  typography : {
+    default : {
+      body : {
+        font : 'default',
+        size : 'fontSize',
+        lineHeight : '1.5',
+        letterSpacing : 'normal',
+        weight : 'normal',
+        style : 'normal',
+        textDecoration : 'none',
+        colour : 'textPrimary',
+      },
     },
   },
 } as const
@@ -89,14 +135,27 @@ function makeSection(section : unknown, { theme } : {
     && ('palette' in section))
       ? section.palette
       : defaultTheme.sections.default.palette;
+  let scale = ((section) && (typeof section === 'object')
+    && ('scale' in section))
+      ? section.scale
+      : defaultTheme.sections.default.scale;
   let background = ((section) && (typeof section === 'object')
     && ('background' in section))
       ? section.background
       : defaultTheme.sections.default.background;
+  let typography = ((section) && (typeof section === 'object')
+    && ('typography' in section))
+      ? section.typography
+      : defaultTheme.sections.default.typography;
 
   if (typeof palette === 'string')
     palette = getPalette(theme, { key : palette });
   else palette = getPalette(theme);
+
+  if (typeof scale === 'string')
+    scale = getScale(theme, { key : scale });
+  else scale = getScale(theme);
+
   if (typeof background === 'string')
     background = getBackground(theme, {
       key : background,
@@ -104,7 +163,23 @@ function makeSection(section : unknown, { theme } : {
     });
   else background = getBackground(theme, { palette : palette as Palette });
 
-  return { palette, background } as Section;
+  const fonts = getFonts(theme);
+  if (typeof typography === 'string')
+    typography = getTypography(theme, {
+      key : typography,
+      palette : palette as Palette,
+      scale : scale as Scale,
+      fonts : fonts as Record<string, Font>,
+    });
+  else {
+    typography = getTypography(theme, {
+      palette : palette as Palette,
+      scale : scale as Scale,
+      fonts : fonts as Record<string, Font>,
+    });
+  }
+
+  return { palette, scale, background, typography } as Section;
 }
 
 function makePalette(palette : unknown) : Palette {
@@ -119,6 +194,44 @@ function makePalette(palette : unknown) : Palette {
   }
 
   return pal;
+}
+
+function makeScale(scale : unknown) : Scale {
+  const scl = ((typeof scale === 'object' && scale !== null)
+    ? Object.entries(scale).reduce((acc, [key, value]) =>
+      ({ ...acc, [key] : value }), {})
+    : {}) as Scale;
+
+  for (const key in scl) {
+    if (typeof scl[key] !== 'string') {
+      delete scl[key];
+    }
+  }
+  return scl;
+}
+
+function makeFonts(fonts : unknown) : Record<string, Font> {
+  const fnt = ((typeof fonts === 'object' && fonts !== null)
+    ? Object.entries(fonts).reduce((acc, [key, value]) =>
+      ({ ...acc, [key] : value }), {})
+    : {}) as Record<string, Font>;
+
+  for (const key in fnt) {
+    const f = fnt[key] = { ...fnt[key] };
+    if (f === null || typeof f !== 'object' || typeof f.family !== 'string') {
+      delete fnt[key];
+      continue;
+    }
+
+    if (f.fontFace !== undefined && typeof f.fontFace !== 'string') {
+      delete f.fontFace;
+    }
+    if (f.import !== undefined && typeof f.import !== 'string') {
+      delete f.import;
+    }
+  }
+
+  return fnt;
 }
 
 function makeBackground(background : unknown, { palette } : {
@@ -183,6 +296,70 @@ function makeBackground(background : unknown, { palette } : {
   return bkg;
 }
 
+function makeTypography(typography : unknown, { palette, scale, fonts } : {
+  palette? : Palette;
+  scale? : Scale;
+  fonts? : Record<string, Font>;
+} = {}) : Record<string, Typography> {
+  const typ = ((typeof typography === 'object' && typography !== null)
+    ? Object.entries(typography).reduce((acc, [key, value]) =>
+      ({ ...acc, [key] : value }), {})
+    : {}) as Record<string, Typography>;
+
+  for (const key in typ) {
+    const t = typ[key] = { ...typ[key] };
+    if (t === null || typeof t !== 'object' || Array.isArray(t)) {
+      delete typ[key];
+      continue;
+    }
+
+    if (typeof t.font !== 'string')
+      t.font = defaultTheme.typography.default.body.font;
+    if (typeof t.size !== 'string')
+      t.size = defaultTheme.typography.default.body.size;
+    if ((t.lineHeight !== undefined) && (typeof t.lineHeight !== 'string'))
+      delete t.lineHeight;
+    if (
+      (t.letterSpacing !== undefined)
+        && (typeof t.letterSpacing !== 'string')
+    ) delete t.letterSpacing;
+    if ((t.weight !== undefined) && (typeof t.weight !== 'string'))
+      delete t.weight;
+    if ((t.style !== undefined) && (typeof t.style !== 'string'))
+      delete t.style;
+    if (
+      (t.textDecoration !== undefined)
+        && (typeof t.textDecoration !== 'string')
+    ) delete t.textDecoration;
+    if ((t.colour !== undefined) && (typeof t.colour !== 'string'))
+      delete t.colour;
+
+    const fontFamily = fonts?.[t.font]?.family;
+    if (fontFamily) t.font = fontFamily;
+    else {
+      const defaultFontFamily = fonts?.default?.family;
+      if (defaultFontFamily) t.font = defaultFontFamily;
+      else t.font = defaultTheme.fonts.default.family;
+    }
+
+    const fontSize = scale?.[t.size];
+    if (fontSize) t.size = fontSize;
+    else {
+      const defaultFontSize = scale?.fontSize;
+      if (defaultFontSize) t.size = defaultFontSize;
+      else t.size = defaultTheme.scales.default.fontSize;
+    }
+
+    if (t.colour) {
+      const paletteColour = palette?.[t.colour];
+      if (paletteColour) t.colour = paletteColour;
+      else delete t.colour;
+    }
+  }
+
+  return typ;
+}
+
 function buildPalette(theme : unknown, key? : string) : object {
   if (!theme || typeof theme !== 'object') return defaultTheme.palettes.default;
 
@@ -197,6 +374,28 @@ function buildPalette(theme : unknown, key? : string) : object {
     : buildPalette(theme, 'default');
 
   return { ...parentPalette, ...palette };
+}
+
+function buildScale(theme : unknown, key? : string) : object {
+  if (!theme || typeof theme !== 'object') return defaultTheme.scales.default;
+
+  if (key === undefined)
+    key = tryGet(theme, '.sections.default.scale', isKey);
+  if (key === undefined) key = 'default';
+  const scale = tryGet(theme, `.scales.${key}`, isObject);
+
+  const parentScale = (key === 'default')
+    ? defaultTheme.scales.default
+    : buildScale(theme, 'default');
+
+  return { ...parentScale, ...scale };
+}
+
+function buildFonts(theme : unknown) : object {
+  if (!theme || typeof theme !== 'object') return defaultTheme.fonts;
+  const fonts = tryGet(theme, '.fonts', isObject);
+  const parentFonts = defaultTheme.fonts;
+  return { ...parentFonts, ...fonts };
 }
 
 function buildBackground(theme : unknown, key? : string) : object {
@@ -214,6 +413,23 @@ function buildBackground(theme : unknown, key? : string) : object {
     : buildBackground(theme, 'default');
 
   return { ...parentBackground, ...background };
+}
+
+function buildTypography(theme : unknown, key? : string) : object {
+  if (!theme || typeof theme !== 'object')
+    return defaultTheme.typography.default;
+
+  if (key === undefined)
+    key = tryGet(theme, '.sections.default.typography', isKey);
+  if (key === undefined) key = 'default';
+
+  const typography = tryGet(theme, `.typography.${key}`, isObject);
+
+  const parentTypography = (key === 'default')
+    ? defaultTheme.typography.default
+    : buildTypography(theme, 'default');
+
+  return { ...parentTypography, ...typography };
 }
 
 function buildSection(theme : unknown, key? : string) : object {
@@ -237,12 +453,40 @@ function getPalette(
   return makePalette(palette);
 }
 
+function getScale(
+  theme : unknown,
+  { key } : { key? : string } = {},
+) : Scale {
+  const scale = buildScale(theme, key);
+  return makeScale(scale);
+}
+
+export function getFonts(
+  theme : unknown,
+) : Record<string, Font> {
+  const fonts = buildFonts(theme);
+  return makeFonts(fonts);
+}
+
 function getBackground(
   theme : unknown,
   { key, palette } : { key? : string, palette? : Palette } = {},
 ) : Background {
   const background = buildBackground(theme, key);
   return makeBackground(background, { palette });
+}
+
+function getTypography(
+  theme : unknown,
+  { key, palette, scale, fonts } : {
+    key? : string;
+    palette? : Palette;
+    scale? : Scale;
+    fonts? : Record<string, Font>;
+  } = {},
+) : Record<string, Typography> {
+  const typography = buildTypography(theme, key);
+  return makeTypography(typography, { palette, scale: scale, fonts });
 }
 
 export function getSection(

systemcarl/blank

src/lib/utils/theme.ts
@@ -2,10 +2,20 @@ import { tryGet } from './typing';
 
 export interface Section {
   palette : Palette;
+  scale : Scale;
   background : Background;
+  typography : Record<string, Typography>;
 }
 
 export type Palette = Record<string, string>;
+
+export type Scale = Record<string, string>;
+
+export interface Font {
+  family : string;
+  fontFace ?: string;
+  import ?: string;
+}
 
 export interface Background {
   img ?: {
@@ -17,21 +27,58 @@ export interface Background {
   fill ?: string;
 }
 
+export interface Typography {
+  font : string;
+  size : string;
+  lineHeight ?: string;
+  letterSpacing ?: string;
+  weight ?: string;
+  style ?: string;
+  textDecoration ?: string;
+  colour ?: string;
+  shadowColour ?: string;
+}
+
 export const defaultTheme = {
   sections : {
     default : {
       palette : 'default',
+      scale : 'default',
       background : 'default',
+      typography : 'default',
     },
   },
   palettes : {
     default : {
       background : '#FFFFFF',
+      textPrimary : '#000000',
     },
+  },
+  scales : {
+    default : {
+      fontSize : '1rem',
+    },
+  },
+  fonts : {
+    default : { family : 'Arial, sans-serif' },
   },
   backgrounds : {
     default : {
       fill : 'background',
+    },
+  },
+  typography : {
+    default : {
+      body : {
+        font : 'default',
+        size : 'fontSize',
+        lineHeight : '1.5',
+        letterSpacing : 'normal',
+        weight : 'normal',
+        style : 'normal',
+        textDecoration : 'none',
+        colour : 'textPrimary',
+      },
     },
   },
 } as const;
@@ -57,14 +104,27 @@ function makeSection(section : unknown, { theme } : {
     && (('palette' in section)
       ? section.palette
       : defaultTheme.sections.default.palette));
+  let scale = ((section) && (typeof section === 'object')
+    && (('scale' in section)
+      ? section.scale
+      : defaultTheme.sections.default.scale));
   let background = ((section) && (typeof section === 'object')
     && (('background' in section)
       ? section.background
       : defaultTheme.sections.default.background));
+  let typography = ((section) && (typeof section === 'object')
+    && (('typography' in section)
+      ? section.typography
+      : defaultTheme.sections.default.typography));
 
   if (typeof palette === 'string')
     palette = getPalette(theme, { key : palette });
   else palette = getPalette(theme);
+
+  if (typeof scale === 'string')
+    scale = getScale(theme, { key : scale });
+  else scale = getScale(theme);
+
   if (typeof background === 'string')
     background = getBackground(theme, {
       key : background,
@@ -72,7 +132,23 @@ function makeSection(section : unknown, { theme } : {
     });
   else background = getBackground(theme, { palette : palette as Palette });
 
-  return { palette, background } as Section;
+  const fonts = getFonts(theme);
+  if (typeof typography === 'string')
+    typography = getTypography(theme, {
+      key : typography,
+      palette : palette as Palette,
+      scale : scale as Scale,
+      fonts : fonts as Record<string, Font>,
+    });
+  else {
+    typography = getTypography(theme, {
+      palette : palette as Palette,
+      scale : scale as Scale,
+      fonts : fonts as Record<string, Font>,
+    });
+  }
+
+  return { palette, scale, background, typography } as Section;
 }
 
 function makePalette(palette : unknown) : Palette {
@@ -87,6 +163,42 @@ function makePalette(palette : unknown) : Palette {
   }
 
   return pal;
+}
+
+function makeScale(scale : unknown) : Scale {
+  const scl = ((typeof scale === 'object' && scale !== null)
+    ? copy(scale)
+    : {}) as Scale;
+
+  for (const key in scl) {
+    if (typeof scl[key] !== 'string') {
+      delete scl[key];
+    }
+  }
+  return scl;
+}
+
+function makeFonts(fonts : unknown) : Record<string, Font> {
+  const fnt = ((typeof fonts === 'object' && fonts !== null)
+    ? copy(fonts)
+    : {}) as Record<string, Font>;
+
+  for (const key in fnt) {
+    const f = fnt[key];
+    if (f === null || typeof f !== 'object' || typeof f.family !== 'string') {
+      delete fnt[key];
+      continue;
+    }
+
+    if (f.fontFace !== undefined && typeof f.fontFace !== 'string') {
+      delete f.fontFace;
+    }
+    if (f.import !== undefined && typeof f.import !== 'string') {
+      delete f.import;
+    }
+  }
+
+  return fnt;
 }
 
 function makeBackground(background : unknown, { palette } : {
@@ -151,6 +263,78 @@ function makeBackground(background : unknown, { palette } : {
   return bkg;
 }
 
+function makeTypography(typography : unknown, { palette, scale, fonts } : {
+  palette ?: Palette;
+  scale ?: Scale;
+  fonts ?: Record<string, Font>;
+} = {}) : Record<string, Typography> {
+  const typ = ((typeof typography === 'object' && typography !== null)
+    ? copy(typography)
+    : {}) as Record<string, Typography>;
+
+  for (const key in typ) {
+    if (typ[key] === undefined) continue;
+    const t = typ[key] = { ...typ[key] };
+    if (t === null || typeof t !== 'object' || Array.isArray(t)) {
+      delete typ[key];
+      continue;
+    }
+
+    if (typeof t.font !== 'string')
+      t.font = defaultTheme.typography.default.body.font;
+    if (typeof t.size !== 'string')
+      t.size = defaultTheme.typography.default.body.size;
+    if ((t.lineHeight !== undefined) && (typeof t.lineHeight !== 'string'))
+      delete t.lineHeight;
+    if (
+      ((t.letterSpacing !== undefined)
+        && (typeof t.letterSpacing !== 'string'))
+    ) delete t.letterSpacing;
+    if ((t.weight !== undefined) && (typeof t.weight !== 'string'))
+      delete t.weight;
+    if ((t.style !== undefined) && (typeof t.style !== 'string'))
+      delete t.style;
+    if (
+      ((t.textDecoration !== undefined)
+        && (typeof t.textDecoration !== 'string'))
+    ) delete t.textDecoration;
+    if ((t.colour !== undefined) && (typeof t.colour !== 'string'))
+      delete t.colour;
+    if ((t.shadowColour !== undefined) && (typeof t.shadowColour !== 'string'))
+      delete t.shadowColour;
+
+    const fontFamily = fonts?.[t.font]?.family;
+    if (fontFamily) t.font = fontFamily;
+    else {
+      const defaultFontFamily = fonts?.default?.family;
+      if (defaultFontFamily) t.font = defaultFontFamily;
+      else t.font = defaultTheme.fonts.default.family;
+    }
+
+    const fontSize = scale?.[t.size];
+    if (fontSize) t.size = fontSize;
+    else {
+      const defaultFontSize = scale?.fontSize;
+      if (defaultFontSize) t.size = defaultFontSize;
+      else t.size = defaultTheme.scales.default.fontSize;
+    }
+
+    if (t.colour) {
+      const paletteColour = palette?.[t.colour];
+      if (paletteColour) t.colour = paletteColour;
+      else delete t.colour;
+    }
+
+    if (t.shadowColour) {
+      const paletteColour = palette?.[t.shadowColour];
+      if (paletteColour) t.shadowColour = paletteColour;
+      else delete t.shadowColour;
+    }
+  }
+
+  return typ;
+}
+
 function buildPalette(theme : unknown, key ?: string) : object {
   if (!theme || typeof theme !== 'object') return defaultTheme.palettes.default;
 
@@ -165,6 +349,28 @@ function buildPalette(theme : unknown, key ?: string) : object {
     : buildPalette(theme, 'default');
 
   return { ...parentPalette, ...palette };
+}
+
+function buildScale(theme : unknown, key ?: string) : object {
+  if (!theme || typeof theme !== 'object') return defaultTheme.scales.default;
+
+  if (key === undefined)
+    key = tryGet(theme, '.sections.default.scale', isKey);
+  if (key === undefined) key = 'default';
+  const scale = tryGet(theme, `.scales.${key}`, isObject);
+
+  const parentScale = (key === 'default')
+    ? defaultTheme.scales.default
+    : buildScale(theme, 'default');
+
+  return { ...parentScale, ...scale };
+}
+
+function buildFonts(theme : unknown) : object {
+  if (!theme || typeof theme !== 'object') return defaultTheme.fonts;
+  const fonts = tryGet(theme, '.fonts', isObject);
+  const parentFonts = defaultTheme.fonts;
+  return { ...parentFonts, ...fonts };
 }
 
 function buildBackground(theme : unknown, key ?: string) : object {
@@ -182,6 +388,23 @@ function buildBackground(theme : unknown, key ?: string) : object {
     : buildBackground(theme, 'default');
 
   return { ...parentBackground, ...background };
+}
+
+function buildTypography(theme : unknown, key ?: string) : object {
+  if (!theme || typeof theme !== 'object')
+    return defaultTheme.typography.default;
+
+  if (key === undefined)
+    key = tryGet(theme, '.sections.default.typography', isKey);
+  if (key === undefined) key = 'default';
+
+  const typography = tryGet(theme, `.typography.${key}`, isObject);
+
+  const parentTypography = (key === 'default')
+    ? defaultTheme.typography.default
+    : buildTypography(theme, 'default');
+
+  return { ...parentTypography, ...typography };
 }
 
 function buildSection(theme : unknown, key ?: string) : object {
@@ -205,12 +428,40 @@ function getPalette(
   return makePalette(palette);
 }
 
+function getScale(
+  theme : unknown,
+  { key } : { key ?: string; } = {},
+) : Scale {
+  const scale = buildScale(theme, key);
+  return makeScale(scale);
+}
+
+export function getFonts(
+  theme : unknown,
+) : Record<string, Font> {
+  const fonts = buildFonts(theme);
+  return makeFonts(fonts);
+}
+
 function getBackground(
   theme : unknown,
   { key, palette } : { key ?: string; palette ?: Palette; } = {},
 ) : Background {
   const background = buildBackground(theme, key);
   return makeBackground(background, { palette });
+}
+
+function getTypography(
+  theme : unknown,
+  { key, palette, scale, fonts } : {
+    key ?: string;
+    palette ?: Palette;
+    scale ?: Scale;
+    fonts ?: Record<string, Font>;
+  } = {},
+) : Record<string, Typography> {
+  const typography = buildTypography(theme, key);
+  return makeTypography(typography, { palette, scale : scale, fonts });
 }
 
 export function getSection(