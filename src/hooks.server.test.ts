systemcarl/folio

Log Request Events
Send structured request data — including method, URL, duration, and
status — to standard output for log egression and debugging. Log URL
and method of unhandled error events.

src/hooks.server.test.ts
@@ -1,6 +1,8 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { beforeEach, describe, it, expect, vi } from 'vitest';
 import * as Sentry from '@sentry/sveltekit'
+
+import { log } from '$lib/utils/logs';
 import { handle, handleError } from './hooks.server';
 
 const sentryHandlerSpy = vi.hoisted(() => vi.fn());
@@ -10,12 +12,30 @@ vi.mock('$env/static/public', () => ({
 }));
 vi.mock('@sentry/sveltekit', () => ({
   init : vi.fn(),
-  sentryHandle : vi.fn(() => (args : unknown) => sentryHandlerSpy(args)),
+  sentryHandle : vi.fn(() => ({ event, resolve } : {
+    event : RequestEvent,
+    resolve : (event : RequestEvent) => Promise<Response> | Response
+  }) => {
+    sentryHandlerSpy({ event, resolve });
+    return resolve(event);
+  }),
   handleErrorWithSentry : vi.fn((handler) => (args : unknown) => {
     sentryHandlerSpy(args);
     return handler(args);
   }),
 }));
+vi.mock('$lib/utils/logs', () => ({
+  log : vi.fn(),
+}));
+
+function stubError() { return new Error('Test error'); }
+function stubEvent() {
+  return {
+    url : new URL('https://example.com/test'),
+    request : { method : 'GET' } as Request,
+  } as RequestEvent;
+}
+function stubResponse() { return new Response('OK', { status : 200 }); }
 
 beforeEach(() => {
   vi.clearAllMocks();
@@ -27,28 +47,75 @@ describe('server hooks', () => {
     await import('./hooks.server');
 
     expect(Sentry.init).toHaveBeenCalledWith({
-      dsn: 'test-dsn',
-      sendDefaultPii: true,
+      dsn : 'test-dsn',
+      sendDefaultPii : true,
     });
   });
 });
 
 describe('request handler', () => {
   it('monitors request with Sentry', async () => {
-    const event = {} as RequestEvent;
-    const resolve = vi.fn();
+    const event = stubEvent();
+    const resolve = vi.fn(() => stubResponse());
 
     await handle({ event, resolve });
 
-    expect(sentryHandlerSpy).toHaveBeenCalledWith({ event, resolve });
+    expect(sentryHandlerSpy)
+      .toHaveBeenCalledWith(expect.objectContaining({ event }));
+  });
+
+  it('logs request response', async () => {
+    const expectedDuration = 500;
+    const dateSpy = vi.spyOn(Date, 'now')
+    dateSpy.mockReturnValueOnce(1000);
+    dateSpy.mockReturnValueOnce(1000 + expectedDuration);
+    const event = stubEvent();
+    const resolve = vi.fn(() => stubResponse());
+
+    await handle({ event, resolve });
+
+    expect(log).toHaveBeenCalledWith({
+      event : {
+        message : 'Request response',
+        url : event.url.pathname,
+        method : event.request.method,
+        duration : expectedDuration,
+        status : 200,
+      },
+      type : 'http',
+    }, {
+      level : 'info',
+    });
+
+    dateSpy.mockRestore();
+  });
+
+  it('resolves the request', async () => {
+    const event = stubEvent();
+    const response = stubResponse();
+    const resolve = vi.fn(() => response);
+
+    await handle({ event, resolve });
+
+    expect(resolve).toHaveBeenCalledOnce();
+    expect(resolve.mock.calls[0]).toEqual([event, expect.anything()]);
+  });
+
+  it('returns the response', async () => {
+    const event = stubEvent();
+    const response = stubResponse();
+    const resolve = vi.fn(() => response);
+
+    const result = await handle({ event, resolve });
+
+    expect(result).toBe(response);
   });
 });
 
 describe('error handler', () => {
   it('catches error with Sentry', () => {
-    vi.spyOn(console, 'error').mockImplementation(() => {});
-    const error = new Error('Test error');
-    const event = {} as RequestEvent;
+    const error = stubError();
+    const event = stubEvent();
 
     handleError({ error, event });
 
@@ -56,15 +123,20 @@ describe('error handler', () => {
   });
 
   it('logs errors', () => {
-    const consoleErrorSpy = vi.spyOn(console, 'error')
-      .mockImplementation(() => {});
-    const error = new Error('Test error');
-    const event = {} as RequestEvent;
+    const error = stubError();
+    const event = stubEvent();
 
     handleError({ error, event });
 
-    expect(consoleErrorSpy)
-      .toHaveBeenCalledWith("Unhandled error (server):", error, event);
-    consoleErrorSpy.mockRestore();
+    expect(log).toHaveBeenCalledWith({
+      error,
+      event : {
+        url : event.url.pathname,
+        method : event.request.method,
+      },
+      type : 'unhandled',
+    }, {
+      level : 'error',
+    });
   });
 });

src/hooks.server.ts
@@ -1,5 +1,8 @@
 import type { RequestEvent } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
+
+import { log } from '$lib/utils/logs';
 import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 
 Sentry.init({
@@ -7,13 +10,48 @@ Sentry.init({
   sendDefaultPii: true,
 });
 
+async function requestHandler({ event, resolve } : {
+  event : RequestEvent,
+  resolve : (event : RequestEvent) => Promise<Response> | Response
+}) {
+  const received = Date.now();
+  const response = await resolve(event);
+
+  log({
+    event : {
+      message : 'Request response',
+      url : event.url.pathname,
+      method: event.request.method,
+      duration : Date.now() - received,
+      status : response.status
+    },
+    type : 'http',
+  }, {
+    level : 'info'
+  });
+
+  return response;
+}
+
 function errorHandler({ error, event } : {
   error : unknown;
   event : RequestEvent;
 }) {
-  console.error('Unhandled error (server):', error, event);
+  log({
+    error,
+    event : {
+      url : event.url.pathname,
+      method: event.request.method,
+    },
+    type : 'unhandled',
+  }, {
+    level : 'error',
+  });
 };
 
 export const handleError = Sentry.handleErrorWithSentry(errorHandler);
 
-export const handle = Sentry.sentryHandle();
+export const handle = sequence(
+  Sentry.sentryHandle(),
+  requestHandler,
+);

systemcarl/blank

src/hooks.server.test.ts
@@ -1,9 +1,31 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { beforeEach, afterAll, describe, it, expect, vi } from 'vitest';
 import * as Sentry from '@sentry/sveltekit';
-import { handle, handleError } from './hooks.server';
+
+import { log } from '$lib/server/logs';
+import { handleError } from './hooks.server';
 
 const sentryHandlerSpy = vi.hoisted(() => vi.fn());
+
+vi.mock('@sveltejs/kit/hooks', () => {
+  return {
+    sequence : vi.fn((...handlers : ((args : {
+      event : RequestEvent;
+      resolve : ((event : RequestEvent) => Promise<Response> | Response);
+    }) => Promise<void>)[]) => {
+      return async ({ event, resolve } : {
+        event : RequestEvent;
+        resolve : (event : RequestEvent) => Promise<Response> | Response;
+      }) => {
+        let response;
+        for (const handler of handlers) {
+          response = await handler({ event, resolve });
+        }
+        return response;
+      };
+    }),
+  };
+});
 
 vi.mock('$env/dynamic/public', () => ({
   env : {
@@ -13,12 +35,30 @@ vi.mock('$env/dynamic/public', () => ({
 }));
 vi.mock('@sentry/sveltekit', () => ({
   init : vi.fn(),
-  sentryHandle : vi.fn(() => (args : unknown) => sentryHandlerSpy(args)),
+  sentryHandle : vi.fn(() => ({ event, resolve } : {
+    event : RequestEvent;
+    resolve : (event : RequestEvent) => Promise<Response> | Response;
+  }) => {
+    sentryHandlerSpy({ event, resolve });
+    return resolve(event);
+  }),
   handleErrorWithSentry : vi.fn(handler => (args : unknown) => {
     sentryHandlerSpy(args);
     return handler(args);
   }),
 }));
+vi.mock('$lib/server/logs', () => ({
+  log : vi.fn(),
+}));
+
+function stubError() { return new Error('Test error'); }
+function stubEvent() {
+  return {
+    url : new URL('https://example.com/test'),
+    request : { method : 'GET' } as Request,
+  } as RequestEvent;
+}
+function stubResponse() { return new Response('OK', { status : 200 }); }
 
 beforeEach(() => {
   vi.resetModules();
@@ -41,20 +81,71 @@ describe('server hooks', () => {
 
 describe('request handler', () => {
   it('monitors request with Sentry', async () => {
-    const event = {} as RequestEvent;
-    const resolve = vi.fn();
+    const event = stubEvent();
+    const resolve = vi.fn(() => stubResponse());
+    const { handle } = await import('./hooks.server');
 
     await handle({ event, resolve });
 
-    expect(sentryHandlerSpy).toHaveBeenCalledWith({ event, resolve });
+    expect(sentryHandlerSpy)
+      .toHaveBeenCalledWith(expect.objectContaining({ event }));
+  });
+
+  it('logs request response', async () => {
+    const expectedDuration = 500;
+    const dateSpy = vi.spyOn(Date, 'now');
+    dateSpy.mockReturnValueOnce(1000);
+    dateSpy.mockReturnValueOnce(1000 + expectedDuration);
+    const event = stubEvent();
+    const resolve = vi.fn(() => stubResponse());
+    const { handle } = await import('./hooks.server');
+
+    await handle({ event, resolve });
+
+    expect(log).toHaveBeenCalledWith({
+      event : {
+        message : 'Request response',
+        url : event.url.pathname,
+        method : event.request.method,
+        duration : expectedDuration,
+        status : 200,
+      },
+      type : 'http',
+    }, {
+      level : 'info',
+    });
+
+    dateSpy.mockRestore();
+  });
+
+  it('resolves the request', async () => {
+    const event = stubEvent();
+    const response = stubResponse();
+    const resolve = vi.fn(() => response);
+    const { handle } = await import('./hooks.server');
+
+    await handle({ event, resolve });
+
+    expect(resolve).toHaveBeenCalled();
+    expect(resolve.mock.calls[0]).toEqual([event]);
+  });
+
+  it('returns the response', async () => {
+    const event = stubEvent();
+    const response = stubResponse();
+    const resolve = vi.fn(() => response);
+    const { handle } = await import('./hooks.server');
+
+    const result = await handle({ event, resolve });
+
+    expect(result).toBe(response);
   });
 });
 
 describe('error handler', () => {
   it('catches error with Sentry', () => {
-    vi.spyOn(console, 'error').mockImplementation(() => {});
-    const error = new Error('Test error');
-    const event = {} as RequestEvent;
+    const error = stubError();
+    const event = stubEvent();
 
     handleError({ error, event });
 
@@ -62,15 +153,20 @@ describe('error handler', () => {
   });
 
   it('logs errors', () => {
-    const consoleErrorSpy = vi.spyOn(console, 'error')
-      .mockImplementation(() => {});
-    const error = new Error('Test error');
-    const event = {} as RequestEvent;
+    const error = stubError();
+    const event = stubEvent();
 
     handleError({ error, event });
 
-    expect(consoleErrorSpy)
-      .toHaveBeenCalledWith('Unhandled error (server):', error, event);
-    consoleErrorSpy.mockRestore();
+    expect(log).toHaveBeenCalledWith({
+      error,
+      event : {
+        url : event.url.pathname,
+        method : event.request.method,
+      },
+      type : 'unhandled',
+    }, {
+      level : 'error',
+    });
   });
 });

src/hooks.server.ts
@@ -1,5 +1,8 @@
 import type { RequestEvent } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
+
+import { log } from '$lib/server/logs';
 import { env } from '$env/dynamic/public';
 
 Sentry.init({
@@ -8,13 +11,48 @@ Sentry.init({
   sendDefaultPii : true,
 });
 
+async function requestHandler({ event, resolve } : {
+  event : RequestEvent;
+  resolve : (event : RequestEvent) => Promise<Response> | Response;
+}) {
+  const received = Date.now();
+  const response = await resolve(event);
+
+  log({
+    event : {
+      message : 'Request response',
+      url : event.url.pathname,
+      method : event.request.method,
+      duration : Date.now() - received,
+      status : response.status,
+    },
+    type : 'http',
+  }, {
+    level : 'info',
+  });
+
+  return response;
+}
+
 function errorHandler({ error, event } : {
   error : unknown;
   event : RequestEvent;
 }) {
-  console.error('Unhandled error (server):', error, event);
+  log({
+    error,
+    event : {
+      url : event.url.pathname,
+      method : event.request.method,
+    },
+    type : 'unhandled',
+  }, {
+    level : 'error',
+  });
 };
 
 export const handleError = Sentry.handleErrorWithSentry(errorHandler);
 
-export const handle = Sentry.sentryHandle();
+export const handle = sequence(
+  Sentry.sentryHandle(),
+  requestHandler,
+);