24fa20b6c4feca85bcd3c4f62353cfaf7e43ac80
Carl Lyons <code@carledwardlyons.ca>
2025-11-06 20:56:08 -0500

    Fix Rewriting of Relative Links With Suffixes
    Remove `.md` extensions from relative links with suffixes such as
    anchors and query parameters.

--- src/lib/server/weblog.test.ts
+++ src/lib/server/weblog.test.ts
@@ -100,9 +100,48 @@ describe('loadArticle', () => {
     expect(actual).toEqual(expected);
   });
 
+  it('removes .md extensions from inline links with anchors', async () => {
+    const markdown = '[Link](./other-article.md#section)';
+    const expected = '[Link](./other-article#section)';
+    fetchResourceMock.mockResolvedValue(markdown);
+
+    const actual = await loadArticle('/base', 'article', { fetch : vi.fn() });
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('removes .md extensions from inline links with query', async () => {
+    const markdown = '[Link](./other-article.md?param=value)';
+    const expected = '[Link](./other-article?param=value)';
+    fetchResourceMock.mockResolvedValue(markdown);
+
+    const actual = await loadArticle('/base', 'article', { fetch : vi.fn() });
+    expect(actual).toEqual(expected);
+  });
+
   it('removes .md extensions from implied links', async () => {
     const markdown = '[Link]: ./other-article.md';
     const expected = '[Link]: ./other-article';
+    fetchResourceMock.mockResolvedValue(markdown);
+
+    const actual = await loadArticle('/base', 'article', { fetch : vi.fn() });
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('removes .md extensions from implied links with anchors', async () => {
+    const markdown = '[Link]: ./other-article.md#section';
+    const expected = '[Link]: ./other-article#section';
+    fetchResourceMock.mockResolvedValue(markdown);
+
+    const actual = await loadArticle('/base', 'article', { fetch : vi.fn() });
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('removes .md extensions from implied links with query', async () => {
+    const markdown = '[Link]: ./other-article.md?param=value';
+    const expected = '[Link]: ./other-article?param=value';
     fetchResourceMock.mockResolvedValue(markdown);
 
     const actual = await loadArticle('/base', 'article', { fetch : vi.fn() });
